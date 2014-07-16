91eb752399a5b8d8b698146ccb763e306ad7bd37
wedens <wedens@eldis.ru>
2014-07-16 10:59:17 +0700

    finished facadde spec

--- src/test/scala/vim/scalacompletion/FacadeSpec.scala
+++ src/test/scala/vim/scalacompletion/FacadeSpec.scala
@@ -4,14 +4,21 @@ import org.specs2.mutable._
 import org.specs2.mock._
 import scala.tools.nsc.interactive.Global
 import scala.reflect.internal.util.SourceFile
+import org.specs2.specification.BeforeExample
 
 trait Api extends Global with CompilerApi
 
-class FacadeSpec extends Specification with Mockito with Before { self =>
+class FacadeSpec extends Specification with Mockito with BeforeExample { self =>
   var compilerApi: Compiler = _
   var completionTypeDetector: CompletionTypeDetector = _
 
   var facade: Facade = _
+
+  val body = """package test
+  class X[T <: A[_]](v: T) {
+
+  }
+  """
 
   def before = {
     compilerApi = mock[Compiler]
@@ -25,11 +32,8 @@ class FacadeSpec extends Specification with Mockito with Before { self =>
     }
   }
 
-  val body = """package test
-  class X[T <: A[_]](v: T) {
+  sequential
 
-  }
-  """
   "facade" should {
     "completion" should {
       "update source" in {
@@ -48,16 +52,42 @@ class FacadeSpec extends Specification with Mockito with Before { self =>
         there was one(completionTypeDetector).detect(anyString, anyInt)
       }
 
-      "call type completion" in  {
+      "call type completion when detector says type" in  {
         completionTypeDetector.detect(anyString, anyInt) returns CompletionType.Type
 
         facade.completeAt(body, 35, 15)
 
-        there was one(compilerApi).typeCompletion(any[scala.reflect.internal.util.Position], any[])
+        there was one(compilerApi).typeCompletion(any[scala.reflect.internal.util.Position], any)
       }
-      "call scope completion" in pending
-      "not call any completion" in pending
-      "return empty seq when no completion" in pending
+
+      "call scope completion when detector says scope" in {
+        completionTypeDetector.detect(anyString, anyInt) returns CompletionType.Scope
+
+        facade.completeAt(body, 35, 15)
+
+        there was one(compilerApi).scopeCompletion(any[scala.reflect.internal.util.Position], any)
+      }
+
+      "not call any completion when detector says no completion" in {
+        completionTypeDetector.detect(anyString, anyInt) returns CompletionType.NoCompletion
+
+        facade.completeAt(body, 35, 15)
+
+        there was one(compilerApi).addSources(any)
+        there were noMoreCallsTo(compilerApi)
+      }
+
+      "return empty seq when no completion" in {
+        completionTypeDetector.detect(anyString, anyInt) returns CompletionType.NoCompletion
+
+        facade.completeAt(body, 35, 15) must be empty
+      }
+
+      "call completion type detector with correct parameters" in {
+        facade.completeAt(body, 35, 15)
+
+        there was one(completionTypeDetector).detect(body.split('\n')(1), 15)
+      }
     }
   }
 }
