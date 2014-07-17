e9fb731a6010a2674bd578e905200d5ebc461db4
wedens <wedens@eldis.ru>
2014-07-17 17:00:35 +0700

    extract member info required for ranking

--- src/main/scala/vim/scalacompletion/MemberInfoExtractor.scala
+++ src/main/scala/vim/scalacompletion/MemberInfoExtractor.scala
@@ -1,6 +1,7 @@
 package vim.scalacompletion
 
 import scala.tools.nsc.interactive.Global
+import scala.tools.nsc.symtab.Flags
 
 case class MemberInfo(name: String, fullSignature: String,
   isConstructor: Boolean = false, isLocal: Boolean = false, isPublic: Boolean = false)
@@ -10,6 +11,8 @@ object MemberInfoExtractor {
     val name = member.sym.nameString
     val fullSignature = member.sym.defStringSeenAs(member.tpe)
     val isConstructor = member.sym.isConstructor
+    val isLocal = member.sym.hasFlag(Flags.LOCAL)
+    val isPublic = member.sym.isPublic
     MemberInfo(name, fullSignature, isConstructor)
   }
 }
