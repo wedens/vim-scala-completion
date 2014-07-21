fa09675d47a9aae96e326abc1ec8dd5bc2cb3feb
wedens <wedens@eldis.ru>
2014-07-21 12:50:01 +0700

    use prefix length in ranking

--- src/main/scala/vim/scalacompletion/MemberRankCalculatorImpl.scala
+++ src/main/scala/vim/scalacompletion/MemberRankCalculatorImpl.scala
@@ -2,13 +2,13 @@ package vim.scalacompletion
 
 object MemberRankCalculatorImpl extends MemberRankCalculator[MemberInfo] {
   def apply(prefix: Option[String], member: MemberInfo): Int = {
-    var rank = 0
+    var rank = 100
 
     if (member.isLocal) rank += 10
     if (member.isPublic) rank += 5
     if (!member.isFromRootObjects) rank += 10
     rank += member.isInherited.filterNot(isInherited => isInherited).map(_ => 10) getOrElse 0
-    rank += prefix.filter(member.name.startsWith(_)).map(_ => 10) getOrElse 0
+    rank -= prefix.map(member.name.length - _.length) getOrElse 0
 
     rank
   }
--- src/test/scala/vim/scalacompletion/MemberRankCalculatorImplSpec.scala
+++ src/test/scala/vim/scalacompletion/MemberRankCalculatorImplSpec.scala
@@ -5,33 +5,40 @@ import org.specs2.mutable.Specification
 class MemberRankCalculatorImplSpec extends Specification {
   val calculatorWithNonePrefix = (MemberRankCalculatorImpl.apply _).curried(None)
 
+  val base = 100
+
   "member rank calculator" should {
-    "start with 0" in {
-      calculatorWithNonePrefix(MemberInfo("a", "")) must_== 0
+    "start with 100" in {
+      calculatorWithNonePrefix(MemberInfo("a", "")) must_== base
     }
 
     "add 10 if member is local" in {
-      calculatorWithNonePrefix(MemberInfo("a", "", isLocal = true)) must_== 10
+      calculatorWithNonePrefix(MemberInfo("a", "", isLocal = true)) must_== base + 10
     }
 
     "add 5 if member is public" in {
-      calculatorWithNonePrefix(MemberInfo("a", "", isPublic = true)) must_== 5
+      calculatorWithNonePrefix(MemberInfo("a", "", isPublic = true)) must_== base + 5
     }
 
     "add 10 if member is not inhereted from Any, Object etc" in {
-      calculatorWithNonePrefix(MemberInfo("a", "", isFromRootObjects = false)) must_== 10
+      calculatorWithNonePrefix(MemberInfo("a", "", isFromRootObjects = false)) must_== base + 10
     }
 
     "add 10 if member is not inhereted from Any, Object etc" in {
-      calculatorWithNonePrefix(MemberInfo("a", "", isInherited = Some(false))) must_== 10
+      calculatorWithNonePrefix(MemberInfo("a", "", isInherited = Some(false))) must_== base + 10
     }
 
-    "add 10 if member name starts with prefix" in {
-      MemberRankCalculatorImpl(Some("qwe"), MemberInfo("qwerty", "")) must_== 10
-    }
+    // Don't need as we filter out members that not starts with prefix
+    // "add 10 if member name starts with prefix" in {
+    //   MemberRankCalculatorImpl(Some("qwerty"), MemberInfo("qwerty", "")) must_== base + 10
+    // }
 
-    "not add 10 if member name starts with prefix" in {
-      MemberRankCalculatorImpl(Some("xx"), MemberInfo("qwerty", "")) must_== 0
+    // "not add 10 if member name starts with prefix" in {
+    //   MemberRankCalculatorImpl(Some("xx"), MemberInfo("qwerty", "")) must_== base
+    // }
+
+    "substract difference between prefix length and member name length" in {
+      MemberRankCalculatorImpl(Some("qwe"), MemberInfo("qwerty", "")) must_== base - 3
     }
   }
 }
