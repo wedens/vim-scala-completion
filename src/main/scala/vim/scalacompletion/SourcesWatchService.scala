ae6863498185666f35ddf70d7b4b9e120a10cbd3
wedens <wedens@eldis.ru>
2014-07-21 15:27:15 +0700

    logging in watch service

--- src/main/scala/vim/scalacompletion/SourcesWatchService.scala
+++ src/main/scala/vim/scalacompletion/SourcesWatchService.scala
@@ -8,8 +8,7 @@ import java.nio.file.WatchEvent.Modifier
 import akka.actor.ActorRef
 import FileSystemEvents._
 
-
-class SourcesWatchService(observerActor: ActorRef) extends Runnable {
+class SourcesWatchService(observerActor: ActorRef) extends Runnable with WithLog {
   private val watchService = FileSystems.getDefault.newWatchService()
 
   def watchRecursively(root: Path) {
@@ -23,23 +22,29 @@ class SourcesWatchService(observerActor: ActorRef) extends Runnable {
   }
 
   private def watch(path: Path) =
-    path.register(watchService, ENTRY_CREATE, ENTRY_DELETE)
+    path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY)
 
   override def run(): Unit = {
-    while (!Thread.currentThread.isInterrupted) {
-      val key = watchService.take()
-      key.pollEvents().foreach { event =>
-        val relativePath = event.context().asInstanceOf[Path]
-        val path = key.watchable().asInstanceOf[Path].resolve(relativePath)
-        val pathAsFile = path.toFile
-        event.kind() match {
-          case ENTRY_CREATE if pathAsFile.isDirectory => watchRecursively(path)
-          case ENTRY_CREATE => observerActor ! Created(pathAsFile)
-          case ENTRY_DELETE => observerActor ! Deleted(pathAsFile)
-          case ENTRY_MODIFY => observerActor ! Modifyed(pathAsFile)
-          case x =>
+    try {
+      while (!Thread.currentThread.isInterrupted) {
+        val key = watchService.take()
+        key.pollEvents().foreach { event =>
+          val relativePath = event.context().asInstanceOf[Path]
+          val path = key.watchable().asInstanceOf[Path].resolve(relativePath)
+          val pathAsFile = path.toFile
+          event.kind() match {
+            case ENTRY_CREATE if pathAsFile.isDirectory => watchRecursively(path)
+            case ENTRY_CREATE => observerActor ! Created(pathAsFile)
+            case ENTRY_DELETE => observerActor ! Deleted(pathAsFile)
+            case ENTRY_MODIFY => observerActor ! Modifyed(pathAsFile)
+            case x => logg.warn(s"Unknown event kind: $x at path $path")
+          }
         }
       }
+    } catch {
+      case e: InterruptedException => logg.info("Source watch service is stopped.")
+    } finally {
+      watchService.close()
     }
   }
 }
