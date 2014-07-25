fa00ef2e9ce55d7d53d18ff5e3713e4d7e45a576
wedens <wedens@eldis.ru>
2014-07-25 12:06:00 +0700

    log error when required port is busy

--- src/main/scala/vim/scalacompletion/api/Boot.scala
+++ src/main/scala/vim/scalacompletion/api/Boot.scala
@@ -16,6 +16,7 @@ object Boot extends App with WithLog {
   val watchService: WatchService = new WatchService()
   val watchServiceThread = new Thread(watchService, "WatchService")
   watchServiceThread.setDaemon(true)
+  //TODO: start after successful initialization
   watchServiceThread.start() //stopped in api actor
 
   val facadeFactory = new FacadeFactoryImpl(watchService)
@@ -26,6 +27,12 @@ object Boot extends App with WithLog {
 
   implicit val bindingTimeout = Timeout(1.second)
   import system.dispatcher
-  val boundFuture = IO(Http) ? Http.Bind(api, "localhost", port = 8085)
-  boundFuture.foreach { _ => logg.info("Application started") }
+  val port = 8085
+  val boundFuture = IO(Http) ? Http.Bind(api, "localhost", port = port)
+  boundFuture onSuccess {
+    case _: Http.Bound => logg.info("Scala completion server started.")
+    case Http.CommandFailed(_: Http.Bind) =>
+      logg.error(s"Unable to start scala completion server. Port $port is occupied by some other process.")
+      system.shutdown()
+  }
 }
