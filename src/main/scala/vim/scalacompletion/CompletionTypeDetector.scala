ad01bf48e3495522f88c49386e8efab6a064d468
wedens <wedens@eldis.ru>
2014-07-25 11:12:36 +0700

    small extract method like refactorings

--- src/main/scala/vim/scalacompletion/CompletionTypeDetector.scala
+++ src/main/scala/vim/scalacompletion/CompletionTypeDetector.scala
@@ -23,46 +23,52 @@ class CompletionTypeDetector extends WithLog {
 
     val (beforePosAndPos, afterPos) = line.splitAt(pos + 1)
     val atPos = beforePosAndPos.last
-    if (pos >= line.length && atPos == '.') return CompletionType.Type
-    val beforePos = beforePosAndPos.init
-    val lineBeforePosReversed = beforePos.reverse
-
-    def isInfix(str: String) = {
-      val wordRemoved = str.dropWhile(!_.isSpaceChar)
-      val somethingBeforeSpace = wordRemoved.length - 1 > 0
-      if (somethingBeforeSpace) {
-        val withoutSpace = wordRemoved.tail
-        val looksLikeIdentifierBeforeSpace = withoutSpace.head.isLetterOrDigit
-        looksLikeIdentifierBeforeSpace
-      } else false
-    }
-
-    val insideOfString = lineBeforePosReversed.count(_ == '"') % 2 != 0
-    if (insideOfString) {
-      lineBeforePosReversed.headOption match {
-        case Some('$') => CompletionType.Scope
-        case _ => CompletionType.NoCompletion
-      }
+    if (pos >= line.length && atPos == '.') {
+      CompletionType.Type
     } else {
-      val withoutSpaces = lineBeforePosReversed.dropWhile(_.isSpaceChar)
-      withoutSpaces.headOption match {
-        // type completion after identifier with following dot
-        case Some('.') => CompletionType.Type
-        // empty line before completion position
-        case None => CompletionType.Scope
-        // scope completion after ';' separator
-        case Some(';') => CompletionType.Scope
-        // after: 'if', 'with' etc
-        case Some(_) if scopeKeywords.exists(withoutSpaces.startsWith(_)) => CompletionType.Scope
-        // inside '{}' in import: import pkg.nest.{}
-        // TODO: better regex
-        case Some(_) if withoutSpaces.matches(".*\\{.* tropmi.*") => CompletionType.Type
-        // complete infix method parameter
-        case Some(_) if isInfix(withoutSpaces) => CompletionType.Scope
-        // complete infix members
-        case Some(ch) if ch.isLetterOrDigit => CompletionType.Type
-        case _ => CompletionType.Scope
+      val beforePos = beforePosAndPos.init
+      val lineBeforePosReversed = beforePos.reverse
+
+      val insideOfString = lineBeforePosReversed.count(_ == '"') % 2 != 0
+      if (insideOfString) {
+        lineBeforePosReversed.headOption match {
+          case Some('$') => CompletionType.Scope
+          case _ => CompletionType.NoCompletion
+        }
+      } else {
+        val trimmed = lineBeforePosReversed.trim
+        trimmed.headOption match {
+          // type completion after identifier with following dot
+          case Some('.') => CompletionType.Type
+          // empty line before completion position
+          case None => CompletionType.Scope
+          // scope completion after ';' separator
+          case Some(';') => CompletionType.Scope
+          // after: 'if', 'with' etc
+          case Some(_) if precedingKeyword(trimmed) => CompletionType.Scope
+          // inside '{}' in import: import pkg.nest.{}
+          case Some(_) if importSelectors(trimmed) => CompletionType.Type
+          // complete infix method parameter
+          case Some(_) if infixParameter(trimmed) => CompletionType.Scope
+          // complete infix members
+          case Some(ch) if ch.isLetterOrDigit => CompletionType.Type
+          case _ => CompletionType.Scope
+        }
       }
     }
   }
+
+
+  private def infixParameter(str: String) = {
+    val wordRemoved = str.dropWhile(!_.isSpaceChar)
+    val somethingBeforeSpace = wordRemoved.length - 1 > 0
+    if (somethingBeforeSpace) {
+      val withoutSpace = wordRemoved.tail
+      val looksLikeIdentifierBeforeSpace = withoutSpace.head.isLetterOrDigit
+      looksLikeIdentifierBeforeSpace
+    } else false
+  }
+
+  private def precedingKeyword(str: String) = scopeKeywords.exists(str.startsWith(_))
+  private def importSelectors(str: String) = str.matches(".*\\{.* tropmi[\\s;]*")
 }
