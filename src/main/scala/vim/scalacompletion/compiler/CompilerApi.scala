e71a8200d9f83d13619a66dfdb8310707908ebf6
wedens <wedens13@yandex.ru>
2014-07-30 00:00:45 +0700

    Compiler class extracted to separate file

--- src/main/scala/vim/scalacompletion/compiler/CompilerApi.scala
+++ src/main/scala/vim/scalacompletion/compiler/CompilerApi.scala
@@ -3,11 +3,7 @@ package vim.scalacompletion.compiler
 import vim.scalacompletion.WithLog
 
 import scala.reflect.internal.util.{SourceFile, BatchSourceFile}
-import scala.tools.nsc.Settings
 import scala.tools.nsc.interactive.Global
-import scala.tools.nsc.reporters.Reporter
-
-class Compiler(settings: Settings, _reporter: Reporter, projectName: String = "") extends Global(settings, _reporter, projectName) with CompilerApi
 
 trait CompilerApi extends WithLog { self: Global =>
 
