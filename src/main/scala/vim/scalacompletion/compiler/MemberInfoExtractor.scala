f11f82ccd448bcb45571fe6b12483139929ddf1f
wedens <wedens13@yandex.ru>
2014-08-09 14:57:00 +0700

    small code formatting

--- src/main/scala/vim/scalacompletion/compiler/MemberInfoExtractor.scala
+++ src/main/scala/vim/scalacompletion/compiler/MemberInfoExtractor.scala
@@ -8,20 +8,20 @@ class MemberInfoExtractorForMemberInfo extends MemberInfoExtractor[MemberInfo] {
   def apply(compiler: Global) = (member: Global#Member) => {
     import compiler.definitions
 
-    val sym = member.sym
-    val name = sym.nameString
+    val sym           = member.sym
+    val name          = sym.nameString
     val fullSignature = member.forceInfoString
     val isConstructor = sym.isConstructor
-    val isLocal = sym.isLocalToBlock
-    val isPublic = sym.isPublic
-    val isInherited = member match {
+    val isLocal       = sym.isLocalToBlock
+    val isPublic      = sym.isPublic
+    val isInherited   = member match {
       case tm: Compiler#TypeMember => Some(tm.inherited)
       case _ => None
     }
 
     val isFromRootObjects = sym.owner == definitions.AnyClass ||
-                           sym.owner == definitions.AnyRefClass ||
-                           sym.owner == definitions.ObjectClass
+                            sym.owner == definitions.AnyRefClass ||
+                            sym.owner == definitions.ObjectClass
 
     val isAccessible = member.accessible
 
